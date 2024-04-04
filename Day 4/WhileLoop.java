2a78c9a6bbc116fa03f9518c7de8ebbd7f98ae39
Om Thakare <om.thakare3217@gmail.com>
2024-04-04 20:02:19 +0530

    Completed Day 4

---
+++ Day 4/WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        
+        while (count < 10) {
+            System.out.println("Count: " + count);
+            count++;
+        }
+    }
+}
