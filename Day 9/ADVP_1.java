8aad460b6fcb76a5154f4487880759dd2a13a6aa
Om Thakare <om.thakare3217@gmail.com>
2024-04-09 20:59:37 +0530

    Completed Day 9

---
+++ Day 9/ADVP_1.java
@@ -0,0 +1,17 @@
+public class ADVP_1 {
+    public static void main(String[] args) {
+        int rows = 5;
+        int columns = 10;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                if (i == 1 || i == rows || j == 1 || j == columns) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
