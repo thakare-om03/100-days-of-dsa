4ec5db5ec3b14130b0c2ebd6ee1e8910e7bd94a1
Om Thakare <om.thakare3217@gmail.com>
2024-04-11 23:05:46 +0530

    Completed Day 11

---
+++ Day 11/ADVP_1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ADVP_1 {
+    public static void drawButterflyPattern(int N) {
+        int space = 2 * N - 1;
+        int star = 0;
+
+        for (int j = 1; j <= 2 * N - 1; j++) {
+            if (j <= N) {
+                space -= 2;
+                star++;
+            } else {
+                space += 2;
+                star--;
+            }
+
+            for (int m = 1; m <= star; m++) {
+                System.out.print("*");
+            }
+
+            for (int n = 1; n <= space; n++) {
+                System.out.print(" ");
+            }
+
+            for (int p = 1; p <= star; p++) {
+                if (p != N) {
+                    System.out.print("*");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of N");
+        int N = sc.nextInt();
+        sc.close();
+
+        drawButterflyPattern(N);
+    }
+}
---
+++ Day 11/ADVP_2.java
@@ -0,0 +1,29 @@
+public class ADVP_2 {
+    public static void main(String[] args) {
+        int n = 5; // number of rows
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
